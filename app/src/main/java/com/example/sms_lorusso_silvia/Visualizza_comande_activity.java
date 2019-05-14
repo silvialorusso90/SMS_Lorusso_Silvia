6da9fdbc4b9d0b73fd4737c8c528847c7486ed79
silvialorusso <silvia.lorusso90@gmail.com>
2019-05-14 16:21:44 +0200

    initial commit

--- app/src/main/java/com/example/sms_lorusso_silvia/Visualizza_comande_activity.java
+++ app/src/main/java/com/example/sms_lorusso_silvia/Visualizza_comande_activity.java
@@ -2,12 +2,24 @@ package com.example.sms_lorusso_silvia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class Visualizza_comande_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visualizza_comande_activity);
+        setContentView(R.layout.activity_visualizza_comande);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rec_v_c);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        //new Downloader_v_c(this, url, recyclerView).execute();
     }
 }
