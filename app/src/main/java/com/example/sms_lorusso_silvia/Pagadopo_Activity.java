b5ab2cafd969ef4880ad148c16862e69b5cf2099
silvialorusso <silvia.lorusso90@gmail.com>
2019-06-27 17:11:28 +0200

    aggiunta di 2 button per il ritorno alla home

--- app/src/main/java/com/example/sms_lorusso_silvia/Pagadopo_Activity.java
+++ app/src/main/java/com/example/sms_lorusso_silvia/Pagadopo_Activity.java
@@ -1,13 +1,17 @@
 package com.example.sms_lorusso_silvia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Pagadopo_Activity extends AppCompatActivity {
 
     TextView a;
+    Button homeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,13 @@ public class Pagadopo_Activity extends AppCompatActivity {
         setTitle("");
 
         a = (TextView) findViewById(R.id.textView7);
+        homeBtn =(Button)findViewById(R.id.btnh);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pagadopo_Activity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sms_lorusso_silvia/Pagato_Activity.java
+++ app/src/main/java/com/example/sms_lorusso_silvia/Pagato_Activity.java
@@ -1,13 +1,17 @@
 package com.example.sms_lorusso_silvia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Pagato_Activity extends AppCompatActivity {
 
     TextView a, b, c;
+    Button homeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,13 @@ public class Pagato_Activity extends AppCompatActivity {
         a = (TextView)findViewById(R.id.textView4);
         b = (TextView)findViewById(R.id.textView5);
         c = (TextView)findViewById(R.id.textView6);
+        homeBtn =(Button)findViewById(R.id.btnH);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Pagato_Activity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
